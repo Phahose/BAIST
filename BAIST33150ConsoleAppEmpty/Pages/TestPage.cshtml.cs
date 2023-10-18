2e8cc707365240a4e08db714e68737c8139ac8ec
Nick <90928460+Phahose@users.noreply.github.com>
2023-10-18 12:48:20 -0600

    Bing property and other stuff done

--- BAIST33150ConsoleAppEmpty/Pages/TestPage.cshtml.cs
+++ BAIST33150ConsoleAppEmpty/Pages/TestPage.cshtml.cs
@@ -5,8 +5,17 @@ namespace BAIST33150ConsoleAppEmpty.Pages
 {
     public class TestPageModel : PageModel
     {
+        public string Message { get; set; } = string.Empty;
+
+        [BindProperty]
+        public string Field { get; set; } = string.Empty;
         public void OnGet()
         {
+            Message = "** OnGet **";
+        }
+        public void OnPost()
+        {
+            Message = "** OnPost **";
         }
     }
 }

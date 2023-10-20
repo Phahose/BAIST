6588bcd745e674e6a9494d0312d038321cfd0300
Nick <90928460+Phahose@users.noreply.github.com>
2023-10-20 11:22:01 -0600

    Form controls added to projet

---
+++ BAIST33150ConsoleAppEmpty/Pages/ConstrolsampleHTML5.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BAIST33150ConsoleAppEmpty.Pages
+{
+    public class ConstrolsampleHTML5Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ BAIST33150ConsoleAppEmpty/Pages/ControlSample.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BAIST33150ConsoleAppEmpty.Pages
+{
+    public class ControlSampleModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

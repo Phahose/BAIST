d0ad68db4871c761175bcf81b61448f3555f1b4b
Nick <ekwomnick@gmail.com>
2023-11-15 12:49:37 -0700

    Changes made ot the stored procedure code

--- ItemsCreationand StoredProcedures.sql
+++ ItemsCreationand StoredProcedures.sql
@@ -18,7 +18,7 @@ ALTER TABLE Item
 	   GetItem done
 	   AddItem done
 	   UpdateItem
-	   DeleteItem
+	   DeleteItem done
 
 	*/
 
@@ -41,15 +41,19 @@ ALTER TABLE Item
 
 					SET @ReturnCode = 0
 				ELSE
-					RAISERROR ('AddProgram - INSERT error: Program table.', 16, 1)
+					RAISERROR ('AddProgram - INSERT error: Item table.', 16, 1)
 			END
 		RETURN @ReturnCode
+
+Drop Procedure AddItem
 
 		/*Get Items*/
 	Create Procedure GetItems
 		As
 		DECLARE @ReturnCode INT
 		SET @ReturnCode = 1
+
+
 
 		Begin 
 			Select 
@@ -61,14 +65,22 @@ ALTER TABLE Item
 
 					SET @ReturnCode = 0
 				ELSE
-					RAISERROR ('AddProgram - INSERT error: Program table.', 16, 1)
+					RAISERROR ('AddProgram - INSERT error: Item table.', 16, 1)
 			END
 		RETURN @ReturnCode
+
+Drop Procedure GetItems
+
+--GetItem
 
  Create Procedure GetItem(@ItemNumber INT)
  As
 	DECLARE @ReturnCode INT
 		SET @ReturnCode = 1
+
+		if @ItemNumber is null
+			RAISERROR('GetItem - Required parameter: @ItemNumber', 16, 1)
+		else
 
 		Begin 
 			Select 
@@ -80,6 +92,59 @@ ALTER TABLE Item
 
 					SET @ReturnCode = 0
 				ELSE
-					RAISERROR ('AddProgram - INSERT error: Program table.', 16, 1)
+					RAISERROR ('AddProgram - INSERT error: Item table.', 16, 1)
 			END
 		RETURN @ReturnCode
+
+Drop Procedure GetItem
+
+Create Procedure DeleteItem(@ItemNumber INT)
+As
+	DECLARE @ReturnCode INT
+		SET @ReturnCode = 1
+
+		if @ItemNumber is null
+		RAISERROR('DeleteItem - Required parameter: @ItemNumber', 16, 1)
+		else
+
+		Begin 
+		Delete From Item
+		Where Item.ItemNumber = @ItemNumber
+		IF @@ERROR = 0
+
+					SET @ReturnCode = 0
+				ELSE
+					RAISERROR ('DeleteItem - INSERT error: Item table.', 16, 1)
+			END
+
+		RETURN @ReturnCode
+
+Drop Procedure DeleteItem
+
+Create Procedure UpdateItem (@ItemNumber INT,@Description Varchar(50), @UnitPrice Money)
+	As 
+	DECLARE @ReturnCode INT
+	SET @ReturnCode = 1
+	if @ItemNumber is null
+	RAISERROR('UpdateItem - Required parameter: @ItemNumber', 16, 1)
+	 else if @Description is Null 
+		RAISERROR('AddItem - Required parameter: @Description', 16, 1)
+	else if @UnitPrice is Null 
+		RAISERROR('AddItem - Required parameter: @UnitPrice', 16, 1)
+	else
+
+		Begin 
+		Update Item
+		Set Description = @Description,
+		    UnitPrice = @UnitPrice
+			Where ItemNumber = @ItemNumber
+			IF @@ERROR = 0
+
+					SET @ReturnCode = 0
+				ELSE
+					RAISERROR ('UpdateItem - INSERT error: Item table.', 16, 1)
+			END
+
+		RETURN @ReturnCode
+
+

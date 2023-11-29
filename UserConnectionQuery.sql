d26d464d058b5edb48657c77c0624fd7a3fe5cda
Nick <ekwomnick@gmail.com>
2023-11-29 12:12:42 -0700

    New user is created in the db so that the application can access the db

--- UserConnectionQuery.sql
+++ UserConnectionQuery.sql
@@ -1,6 +1,11 @@
+-- IIS web user IIS APPPOOL\nekwom1AppPool
+-- IIS automatially adds the IIS_IUSRS membership to the worker process token at runtime
+
 use nekwom1
 
 sp_helpuser
+
+Create User aspnetcore For Login [BUILTIN\IIS_IUSRS]
 
 CREATE PROCEDURE GetDatabaseUser
 AS
@@ -9,4 +14,13 @@ AS
 
 	SELECT CURRENT_USER AS CurrentUser, -- return the name of the current user 
 			SYSTEM_USER AS SystemUser, -- returns the name of the currently executing context,
-			SESSION_USER AS SessionUser, -- returns the name of the current context in the current databased
+			SESSION_USER AS SessionUser -- returns the name of the current context in the current databased
+
+	IF @@ERROR = 0
+		SET @Returncode = 0
+	ELSE 
+		RaisError('GetDatabaseUser - Database User Error.',16,1)
+
+	Return @ReturnCode
+
+	Exec GetDatabaseUser

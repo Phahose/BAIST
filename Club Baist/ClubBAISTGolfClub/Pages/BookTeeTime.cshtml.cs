9a9e22825215670fd16cf6d93bb5d89b4eebf25a
Nick <ekwomnick@gmail.com>
2024-02-01 14:00:14 -0700

    The days in month isssue fixed

--- Club Baist/ClubBAISTGolfClub/Pages/BookTeeTime.cshtml.cs
+++ Club Baist/ClubBAISTGolfClub/Pages/BookTeeTime.cshtml.cs
@@ -19,7 +19,7 @@ namespace ClubBAISTGolfClub.Pages
         [BindProperty]
         public int Month { get; set; } = 4;
         [BindProperty]
-        public int Day { get; set; }
+        public int Day { get; set; } = 1;
         public DateTime Date { get; set; }
         public string TextDate { get; set; }
         public string MonthName { get; set; }
@@ -66,8 +66,8 @@ namespace ClubBAISTGolfClub.Pages
             {
                 Month = today.Month;
             }
-            DateTime firstDayOfMonth = new DateTime(today.Year, today.Month, 1);
-            int daysInMonth = DateTime.DaysInMonth(today.Year, today.Month);
+            DateTime firstDayOfMonth = new DateTime(today.Year, Month, 1);
+            int daysInMonth = DateTime.DaysInMonth(today.Year, Month);
             int currentDay = 1;
             MonthName = GetMonthName(Month);
             Calendar = new int[6, 7];
@@ -95,7 +95,7 @@ namespace ClubBAISTGolfClub.Pages
             Member = memberControlls.GetMember(Email);
             DateTime today = DateTime.Today;
             DateTime firstDayOfMonth = new DateTime(today.Year, Month, 1);
-            int daysInMonth = DateTime.DaysInMonth(today.Year, today.Month);
+            int daysInMonth = DateTime.DaysInMonth(today.Year, Month);
             int currentDay = 1;
 
             Calendar = new int[6, 7];

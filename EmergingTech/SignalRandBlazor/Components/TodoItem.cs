e9a85cd374a2bb6db8d941d589549c72c8bbd760
Nick <ekwomnick@gmail.com>
2024-03-12 14:27:15 -0600

    Todo list done

---
+++ EmergingTech/SignalRandBlazor/Components/TodoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace SignalRandBlazor.Components
+{
+    public class TodoItem
+    {
+        public string Title { get; set; }
+        public bool IsCompleted { get; set; }
+        public bool IsDone { get; set; }
+    }
+}

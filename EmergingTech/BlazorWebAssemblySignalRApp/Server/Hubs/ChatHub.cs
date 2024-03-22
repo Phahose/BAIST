b3a29f33e042e7756a5090da4cfb43e2b4a4dd99
Phahose <ekwomnick@gmail.com>
2024-03-22 12:18:53 -0600

    The Change Made in the Balsor Page

--- EmergingTech/BlazorWebAssemblySignalRApp/Server/Hubs/ChatHub.cs
+++ EmergingTech/BlazorWebAssemblySignalRApp/Server/Hubs/ChatHub.cs
@@ -1,11 +1,12 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿// ChatHub.cs
+using Microsoft.AspNetCore.SignalR;
 
 namespace BlazorWebAssemblySignalRApp.Server.Hubs;
 
 public class ChatHub : Hub
 {
-    public async Task SendMessage(string user, string message)
+    public async Task SendMessage(string firstname, string lastname, string category, string message, DateOnly dateCreated) // Modify the method signature to accept DateCreated as a DateOnly
     {
-        await Clients.All.SendAsync("ReceiveMessage", user, message);
+        await Clients.All.SendAsync("ReceiveMessage", firstname, lastname, category, message, dateCreated); // Pass DateCreated to the ReceiveMessage method
     }
 }
